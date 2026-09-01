a423288dfdd81b98eba06b8b53f6dadfb911c6c1
Dmytro Tarakanov <tarakanovds@gmail.com>
2026-09-01 09:25:43 +0000

    Add health and readiness endpoints

--- app.py
+++ app.py
@@ -13,10 +13,25 @@ app = FastAPI(
 
 @app.get("/", response_class=PlainTextResponse)
 def root():
-    pod_name    = os.getenv("HOSTNAME", "unknown")
+    pod_name = os.getenv("HOSTNAME", "unknown")
     app_message = os.getenv("APP_MESSAGE", "Hello")
 
     return f"{app_message} | Pod: {pod_name}\n"
+
+
+@app.get("/health", response_class=PlainTextResponse)
+def health():
+    return "Healthy\n"
+
+
+@app.get("/ready", response_class=PlainTextResponse)
+def ready():
+    app_secret = os.getenv("APP_SECRET")
+
+    if not app_secret:
+        return PlainTextResponse("Not ready\n", status_code=503)
+
+    return PlainTextResponse("Ready\n", status_code=200)
 
 
 @app.get("/secret", response_class=PlainTextResponse)
