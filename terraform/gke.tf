ecfbd9e397eeb02b83b9a437c3f5ac3f51d38d2d
Dmytro Tarakanov <tarakanovds@gmail.com>
2026-09-15 12:07:43 +0000

    Add GKE Autopilot cluster

---
+++ terraform/gke.tf
@@ -0,0 +1,16 @@
+resource "google_container_cluster" "autopilot" {
+  name     = "python-k8s-app"
+  location = var.region
+
+  enable_autopilot    = true
+  deletion_protection = false
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  depends_on = [
+    google_project_service.required["compute.googleapis.com"],
+    google_project_service.required["container.googleapis.com"],
+  ]
+}
